d1da82bd736ad0f95ecd643f75f46b85d69e8e48
Franx Junior <franklinjunior021118@gmail.com>
2023-08-01 14:36:32 -0500

    corregir data

--- dist/seeders/products460.js
+++ dist/seeders/products460.js
@@ -16,74 +16,74 @@ exports.SeedProducts = void 0;
 const Products_1 = __importDefault(require("../models/Products"));
 const dat = [
     {
-        "nombre": "Camiseta básica blanca",
-        "img": "https://res.cloudinary.com/dw4ebpypt/image/upload/v1690869435/polo_blanco_ug6oan.jpg",
+        nombre: "Camiseta básica blanca",
+        img: "https://res.cloudinary.com/dw4ebpypt/image/upload/v1690869435/polo_blanco_ug6oan.jpg",
         "precio": "15.00",
-        "descripcion": " Esta camiseta básica de algodón en color blanco es una prenda imprescindible en tu colección de ropa. Su diseño sencillo y versátil te permite combinarla fácilmente con cualquier outfit, ya sea para un look casual o más elegante. ",
+        "descripcion": "Esta camiseta básica de algodón en color blanco es una prenda imprescindible en tu colección de ropa. Su diseño sencillo y versátil te permite combinarla fácilmente con cualquier outfit, ya sea para un look casual o más elegante. ",
         "stock": 50
     },
     {
-        "nombre": "Camiseta estampada",
-        "img": "https://res.cloudinary.com/dw4ebpypt/image/upload/v1690869434/camiseta_estampa_ykn78o.jpg",
-        "precio": "20.00",
-        "descripcion": "Agrega un toque de estilo y originalidad a tu look con esta camiseta estampada. Su diseño con estampado y cuello redondo la hace resaltar entre las demás. Perfecta para looks informales y relajados, te brinda comodidad y estilo al mismo tiempo. Su tejido de calidad asegura una larga durabilidad. No te pierdas la oportunidad de adquirir esta prenda con un stock limitado.",
+        nombre: "Camiseta estampada",
+        img: "https://res.cloudinary.com/dw4ebpypt/image/upload/v1690869434/camiseta_estampa_ykn78o.jpg",
+        precio: "20.00",
+        descripcion: "Agrega un toque de estilo y originalidad a tu look con esta camiseta estampada. Su diseño con estampado y cuello redondo la hace resaltar entre las demás. Perfecta para looks informales y relajados.",
         "stock": 30
     },
     {
-        "nombre": "Pantalones vaqueros azules",
-        "img": "https://res.cloudinary.com/dw4ebpypt/image/upload/v1690869434/pantalon_vaquerp_zedog7.jpg",
-        "precio": "40.00",
-        "descripcion": " Estos pantalones vaqueros de estilo clásico en color azul son un básico atemporal en tu vestuario. Su diseño versátil te permite usar para diversas ocasiones. Su ajuste cómodo y su tejido resistente aseguran que se pueden usar durante mucho tiempo. Combínalos con tus camisetas o blusas favoritas para crear diferentes looks. Asegúrese de tenerlos en su armario antes de que se agoten.",
-        "stock": 20
+        nombre: "Pantalones vaqueros azules",
+        img: "https://res.cloudinary.com/dw4ebpypt/image/upload/v1690869434/pantalon_vaquerp_zedog7.jpg",
+        precio: "40.00",
+        descripcion: " Estos pantalones vaqueros de estilo clásico en color azul son un básico atemporal en tu vestuario. Su diseño versátil te permite usar para diversas ocasiones. Su ajuste cómodo y su tejido resistente aseguran que se pueden usar durante mucho tiempo.",
+        stock: 20
     },
     {
-        "nombre": "Vestido de verano",
-        "img": "https://res.cloudinary.com/dw4ebpypt/image/upload/v1690869959/rBVap2DfRRyAQLiSAAEHSoTIMXU249.jpg_xnyjg7.webp",
-        "precio": "50.00",
-        "descripcion": "Prepárate para el verano con este encantador vestido de tirantes. Su diseño ligero y fresco te mantiene cómodo durante los días calurosos. Ideal para paseos en la playa, salidas con amigos o eventos al aire libre. Con su elegante estilo y precio asequible, es una opción perfecta para disfrutar de la temporada estival con estilo.",
+        nombre: "Vestido de verano",
+        img: "https://res.cloudinary.com/dw4ebpypt/image/upload/v1690869959/rBVap2DfRRyAQLiSAAEHSoTIMXU249.jpg_xnyjg7.webp",
+        precio: "50.00",
+        descripcion: "Prepárate para el verano con este encantador vestido de tirantes. Su diseño ligero y fresco te mantiene cómodo durante los días calurosos. Ideal para paseos en la playa, salidas con amigos o eventos al aire libre.",
         "stock": 15
     },
     {
-        "nombre": "Chaqueta de cuero",
-        "img": "https://res.cloudinary.com/dw4ebpypt/image/upload/v1690870067/Chaqueta-en-cuero-sintetico---Negro---H-M-CL_ubcbgy.jpg",
-        "precio": "80.00",
-        "descripcion": "Prepárate para el verano con este encantador vestido de tirantes. Su diseño ligero y fresco te mantiene cómodo durante los días calurosos. Ideal para paseos en la playa, salidas con amigos o eventos al aire libre. Con su elegante estilo y precio asequible, es una opción perfecta para disfrutar de la temporada estival con estilo.",
+        nombre: "Chaqueta de cuero",
+        img: "https://res.cloudinary.com/dw4ebpypt/image/upload/v1690870067/Chaqueta-en-cuero-sintetico---Negro---H-M-CL_ubcbgy.jpg",
+        precio: "80.00",
+        descripcion: "Prepárate para el verano con este encantador vestido de tirantes. Su diseño ligero y fresco te mantiene cómodo durante los días calurosos. Ideal para paseos en la playa, salidas con amigos o eventos al aire libre.",
         "stock": 25
     },
     {
-        "nombre": "poleras con capucha",
-        "img": "https://res.cloudinary.com/dw4ebpypt/image/upload/v1690870122/sudadera_mujer_uniformes_rmsbee.jpg",
-        "precio": "35.00",
-        "descripcion": "Esta polera con capucha es perfecta para tus actividades deportivas o para lucir un look casual y cómodo. Su diseño moderno y bolsillo delantero le dan un toque especial. El tejido de alta calidad asegura comodidad durante todo el día. Aproveche su disponibilidad limitada y llévatela antes de que se agote.",
-        "stock": 40
+        nombre: "poleras con capucha",
+        img: "https://res.cloudinary.com/dw4ebpypt/image/upload/v1690870122/sudadera_mujer_uniformes_rmsbee.jpg",
+        precio: "35.00",
+        descripcion: "Esta polera con capucha es perfecta para tus actividades deportivas o para lucir un look casual y cómodo. Su diseño moderno y bolsillo delantero le dan un toque especial. El tejido de alta calidad asegura comodidad durante todo el día. ",
+        stock: 40
     },
     {
-        "nombre": "Falda plisada",
-        "img": "https://res.cloudinary.com/dw4ebpypt/image/upload/v1690870175/20303c.89_rwujtc.jpg",
-        "precio": "30.00",
-        "descripcion": "Esta elegante falda plisada en color blanco es una prenda versátil y aumentará para añadir a tu armario. Su diseño plisado y corte favorecedor le dan un toque femenino y encantador. Perfecta para ocasiones formales o eventos especiales.",
-        "stock": 10
+        nombre: "Falda plisada",
+        img: "https://res.cloudinary.com/dw4ebpypt/image/upload/v1690870175/20303c.89_rwujtc.jpg",
+        precio: "30.00",
+        descripciom: "Esta elegante falda plisada en color blanco es una prenda versátil y aumentará para añadir a tu armario. Su diseño plisado y corte favorecedor le dan un toque femenino y encantador. Perfecta para ocasiones formales o eventos especiales.",
+        stock: 10
     },
     {
-        "nombre": "Camisa de rayas",
-        "img": "https://res.cloudinary.com/dw4ebpypt/image/upload/v1690870233/0199495401_1_1_16_tv3dlj.webp",
-        "precio": "25.00",
-        "descripcion": "Esta elegante falda plisada en color blanco es una prenda versátil y aumentará para añadir a tu armario. Su diseño plisado y corte favorecedor le dan un toque femenino y encantador.",
-        "stock": 35
+        nombre: "Camisa de rayas",
+        img: "https://res.cloudinary.com/dw4ebpypt/image/upload/v1690870233/0199495401_1_1_16_tv3dlj.webp",
+        precio: "25.00",
+        descripcion: "Esta elegante falda plisada en color blanco es una prenda versátil y aumentará para añadir a tu armario. Su diseño plisado y corte favorecedor le dan un toque femenino y encantador.",
+        stock: 35
     },
     {
-        "nombre": "Zapatos de cuero",
-        "img": "https://res.cloudinary.com/dw4ebpypt/image/upload/v1690870275/Dcuero2-10-scaled-1_upf9gk.webp",
-        "precio": "60.00",
-        "descripcion": "Esta elegante falda plisada en color blanco es una prenda versátil y aumentará para añadir a tu armario. Su diseño plisado y corte favorecedor le dan un toque femenino y encantador. Perfecta para ocasiones formales o eventos especiales.",
-        "stock": 18
+        nombre: "Zapatos de cuero",
+        img: "https://res.cloudinary.com/dw4ebpypt/image/upload/v1690870275/Dcuero2-10-scaled-1_upf9gk.webp",
+        precio: "60.00",
+        descripcion: "Esta elegante falda plisada en color blanco es una prenda versátil y aumentará para añadir a tu armario. Su diseño plisado y corte favorecedor le dan un toque femenino y encantador. Perfecta para ocasiones formales o eventos especiales.",
+        stock: 18
     },
     {
-        "nombre": "Gorro de lana",
-        "img": "https://res.cloudinary.com/dw4ebpypt/image/upload/v1690870350/40a86b46859eba4f04a9bc2a29bdf3a9_kgwcm7.jpg",
-        "precio": "13.00",
-        "descripcion": " Mantén tu cabeza abrigada con este gorro de lana suave y cálido en color marrón. Perfecto para los días fríos de invierno, este gorro es un accesorio imprescindible para complementar tus outfits de temporada.",
-        "stock": 50
+        nombre: "Gorro de lana",
+        img: "https://res.cloudinary.com/dw4ebpypt/image/upload/v1690870350/40a86b46859eba4f04a9bc2a29bdf3a9_kgwcm7.jpg",
+        precio: "13.00",
+        descripcion: " Mantén tu cabeza abrigada con este gorro de lana suave y cálido en color marrón. Perfecto para los días fríos de invierno, este gorro es un accesorio imprescindible para complementar tus outfits de temporada.",
+        stock: 50
     }
 ];
 const SeedProducts = () => __awaiter(void 0, void 0, void 0, function* () {
